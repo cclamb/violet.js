48877d087943d88e20dfcf15020b9686631285da
cclamb <chrislambistan@gmail.com>
2013-12-01 16:12:07 -0700

    some output in buffer

--- bin/buffer.js
+++ bin/buffer.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
+var __ = require('underscore');
 
 var my_buf = new Buffer(10);
 
+console.log('after initialization...');
+
+__.each(my_buf, function(e) { console.log(e.toString(16)); });
+
 console.log(my_buf);
+
+for(var i = 0; i < my_buf.length; i++) {
+	my_buf[i] = 0x00;
+}
+
+console.log('after alteration...')
+
+__.each(my_buf, function(e) { console.log(e.toString(16)); });
+
+console.log(my_buf);
+
+console.log('finished.');
