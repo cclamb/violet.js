ad70b40f583a0f13e74c4ed56e1d7dde931b30be
Chris lamb <cclamb@ece.unm.edu>
2013-12-06 11:02:12 -0700

    serializing to string and b64

--- bin/encoder.js
+++ bin/encoder.js
@@ -1,22 +1,51 @@
 #!/usr/bin/env node
 
 var __ 			= require('underscore'),
-	TextDecoder	= require('stringencoding').TextDecoder,
-	TextEncoder	= require('stringencoding').TextEncoder,
-	values 		= [1, 232, 12, 123, 237],
+	btoa		= require('btoa'),
+	atob		= require('atob'),
+	values 		= [0, 1, 128, 254, 255],
 	buffer 		= new ArrayBuffer(values.length),
 	uint8array 	= new Uint8Array(buffer),
-	encoding	= 'UTF-8';
+	encoding	= 'utf-8';
+
+var toString = function(array) {
+	var string = '';
+
+	__.each(array, function(e) { string += String.fromCharCode(e); });
+	return string;
+}
+
+var fromString = function(string) {
+	var buffer 		= new ArrayBuffer(string.length), 
+		uint8array 	= new Uint8Array(buffer);
+
+	__.each(string, function(e, i) { uint8array[i] = string.charCodeAt(i); }); 
+	return uint8array;
+}
 
 __.each(uint8array, function(e, i, l) { l[i] = values[i]; });
 
-var result = TextDecoder(encoding).decode(uint8array);
-var array = TextEncoder(encoding).encode(result);
+var result = toString(uint8array);
+var b64 = btoa(result);
 
-console.log('The resuting string:');
+var orig = atob(b64);
+var array = fromString(orig);
+
+console.log('==> The original array:');
+console.log(uint8array);
+
+console.log('==> The resuting string:');
 console.log(result);
-console.log('Encoded into the original array:');
+
+console.log('==> values:');
+__.each(result, function(e) {console.log(e.charCodeAt(0)); });
+
+console.log('==> The base64 encoded string:');
+console.log(b64);
+
+
+console.log('==> The string converted from base64:');
+console.log(orig);
+
+console.log('==> Encoded into the original array:');
 console.log(array);
-
-
-
